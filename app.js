5d620b7b12138d9e51cc86cd684b2ebf7092731d
Md Saidul Islam <saiduli066@gmail.comm>
2023-03-04 12:30:54 +0600

    modal is almost done

--- app.js
+++ app.js
@@ -46,15 +46,14 @@ const showCards = (aiTools) => {
             <img src="${image}" class="card-img-top img-fluid" style="width: 450px; height: 250px;" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Features</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
-                  
-                 <div class="d-flex justify-content-between align-items-center">   
-                <div >
-                    <h5 class="card-title">${name}</h5>
-                    <p id="feature-text" class="card-text m-0 p-0">1.${features ? features[0] : "unavailable"}</p>
+                 <p id="feature-text" class="card-text m-0 p-0">1.${features ? features[0] : "unavailable"}</p>
                     <p class="card-text m-0 p-0">2.${features ? features[1] : "unavailable"}</p>
-                    <p class="card-text m-0 p-0">3.${features ? features[2] : ""}</p>
+                    <p class="card-text m-0 p-0">3.${features ? features[2] : "not available"}</p>
+                  
+                 <div class="d-flex justify-content-between align-items-end">   
+                <div >
+                    <h5 class="card-title m-2">${name}</h5>
+                   
                     <div><i class="far fa-calendar-alt"></i> ${published_in}</div>
                 </div>
                 <div>
@@ -79,12 +78,47 @@ const loadSingleData = (id) => {
     const url = ` https://openapi.programming-hero.com/api/ai/tool/${id}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => showSingleData(data.data))
     
     
 }
 
-const showSingleData = () => {
+const showSingleData = (details) => {
+    const Title = document.getElementById('title');
+    //
+    const { description, pricing, features, integrations, image_link } = details;
+
+    console.log(image_link);
+    
+    Title.innerText = `${description}`;
+
+    document.getElementById('month-basic-price').innerText = `${pricing[0] ? pricing[0].price:'free'}`;
+    document.getElementById('month-basic-plan').innerText = `${pricing[0] ? pricing[0].plan : 'free'}`;
+    document.getElementById('month-pro-price').innerText = `${pricing[1] ? pricing[1].price : 'free'}`;
+    document.getElementById('month-pro-plan').innerText = `${pricing[1] ? pricing[1].plan : "free"}`;
+    document.getElementById('enterprise-contact').innerText = `${pricing[2].price}`;
+    document.getElementById('enterprise-section').innerText = `${pricing[2].plan}`;
+    
+    
+    
+    
+    document.getElementById('feature-1').innerText = `${features['1']['feature_name']}`;
+    document.getElementById('feature-2').innerText = `${features['2']['feature_name']}`;
+    document.getElementById('feature-3').innerText = `${features['3']
+    ['feature_name']}`;
+
+    for (let i = 0; i < integrations.length; i++) {
+        let index = i + 1;
+        document.getElementById(`integration-${index}`).innerText = integrations[i];
+       
+    }
+
+    document.getElementById('img-container').innerHTML = `
+        <img class="w-100" style="" src='${image_link[0]}' alt="">
+    `;
+
+
+
     
 }
 
